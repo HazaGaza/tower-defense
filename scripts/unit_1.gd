0a982db573b62d145cc9cdc4ac2b899f608fdd36
runc0 <22boonat@ths.school.nz>
2024-07-04 10:52:21 +1200

    tracking to mouse
    :)

--- scripts/unit_1.gd
+++ scripts/unit_1.gd
@@ -1,15 +1,18 @@
-extends Sprite2D
+extends "res://scripts/Turrets.gd"
 
+func _init():
+	pass
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_range_area_entered(area):
 	print("Sigma!")
+
+
+
+func _on_range_area_exited(area):
+	pass # Replace with function body.
