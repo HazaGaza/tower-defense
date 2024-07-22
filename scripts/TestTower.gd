66dfd11cc99d8d902ca259d17c76e14d6e67c490
runc0 <22boonat@ths.school.nz>
2024-07-22 20:46:04 +1200

    AAAAAAAAAAAAAHHHHHHHHHHHH

--- scripts/TestTower.gd
+++ scripts/TestTower.gd
@@ -1,4 +1,6 @@
-extends PhysicsBody2D
+extends Node2D
+
+var enemy_array = []
 
 func _physics_process(delta):
 	turn()
@@ -6,3 +8,14 @@ func _physics_process(delta):
 func turn():
 	var enemy_position = get_global_mouse_position()
 	get_node("TowerHead").look_at(enemy_position)
+
+
+
+func _on_sight_body_entered(Area2D):
+	enemy_array.append(Area2D.get_parent())
+	print(enemy_array)
+
+
+func _on_sight_body_exited(body):
+	enemy_array.erase(body.get_parent())
+
