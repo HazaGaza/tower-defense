9be1542579dad432d80459a6351eadec62728bf4
runc0 <22boonat@ths.school.nz>
2024-07-26 00:59:10 +1200

    spritesnstuff

--- scripts/enemy1.gd
+++ scripts/enemy1.gd
@@ -4,7 +4,20 @@ func _ready():
 	add_to_group("enemy")
 # Called when the node enters the scene tree for the first time.
 
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
+
+var TimeInSeconds = 5
+
+func _process(delta):
+	anim.play("Fly right")
+	if TimeInSeconds > 0:
+		TimeInSeconds -= delta
+		self.position.x += 1 * delta
+
+
+
 func _on_area_entered(area):
 	if area.is_in_group("bullet"):
 		area.queue_free()
 		queue_free()
+
