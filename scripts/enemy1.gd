c305b6d808c9cc4e68d6683324d44017fb7222f7
runc0 <22boonat@ths.school.nz>
2024-07-26 15:11:21 +1200

    !!

--- scripts/enemy1.gd
+++ scripts/enemy1.gd
@@ -2,14 +2,8 @@ extends Area2D
 class_name enemy
 func _ready():
 	add_to_group("enemy")
+	get_node("AnimatedSprite2D").play("Fly right")
 # Called when the node enters the scene tree for the first time.
-
-@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
-
-
-
-func _process(delta):
-	anim.play("Fly right")
 
 
 func _on_area_entered(area):
@@ -17,7 +11,9 @@ func _on_area_entered(area):
 		area.queue_free()
 		queue_free()
 
-
-
 func _on_timer_timeout():
+	self.wait__time = 2
+	get_node("AnimatedSprite2D").rotation_degrees -= 90
+	get_node("AnimatedSprite2D").play("Fly down")
+	get_node("AnimatedSprite2D").play("Fly right")
 	
