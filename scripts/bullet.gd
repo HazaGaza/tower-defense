212eaaa0c2188dd12de543b934d965d567e9566c
HazaGaza <22leahar@ths.school.nz>
2024-08-16 12:29:28 +1200

    Update bullet.gd

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -5,8 +5,6 @@ func _physics_process(delta):
 	position += transform.x * speed * delta
 func _ready():
 	add_to_group("bullet")
-	get_node("ArrowProjectile").rotation_degrees += 90
-	rotation_degrees += 90
 func _on_body_entered(body):
 	if body.is_in_group("enemy"):
 		self.queue_free()
