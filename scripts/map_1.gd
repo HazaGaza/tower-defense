59eeea79e184aeeb810de6c465680995824a0483
runc0 <22boonat@ths.school.nz>
2024-08-23 13:30:36 +1200

    sigma

--- scripts/map_1.gd
+++ scripts/map_1.gd
@@ -3,20 +3,20 @@ var ui = preload("res://Prefabs/unit_ui.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+<<<<<<< Updated upstream
 	get_node("Corner1").add_to_group("corner")
 	var superui = ui.instantiate()
 	add_child(superui)
 	
 
+=======
+	pass
+>>>>>>> Stashed changes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	pass
 
-
-func _on_corner_1_body_entered(body):
-	pass
-
-
 func _on_end_body_entered(body):
 	body.queue_free()
+
