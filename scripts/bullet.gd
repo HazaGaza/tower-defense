546105b1cfe3a6c21c07f9906000c60872963924
HazaGaza <22leahar@ths.school.nz>
2024-08-23 12:34:43 +1200

    sigma

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,7 +2,7 @@ extends Area2D
 class_name bullet
 var speed = 750
 func _physics_process(delta):
-	position -= transform.y * speed * delta
+	position -= transform.x * speed * delta
 func _ready():
 	add_to_group("bullet")
 	get_node("Sprite2D").rotation_degrees += 90
