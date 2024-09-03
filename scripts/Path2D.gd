814e7fe3667ced9e7e78a92ecf0ea7f1e89bf59d
runc0 <22boonat@ths.school.nz>
2024-09-03 19:49:23 +1200

    waves stuff

--- scripts/Path2D.gd
+++ scripts/Path2D.gd
@@ -4,17 +4,26 @@ class_name enemy_path
 @onready var path_prefab1 = preload("res://Prefabs/enemy_2.tscn")
 var current_wave = 0
 var enemies_in_wave = 0
-# Called when the node enters the scene tree for the first time.
+# Define the wave data, where each element is a list containing the enemy type and delay
+var waves = [
+	[["enemy", 0.7], ["enemy_2", 0.1]],  # Wave 1
+	[["enemy", 0.5], ["enemy", 0.5], ["enemy_2", 0.2]],  # Wave 2
+	[["enemy_2", 0.3], ["enemy", 0.4], ["enemy_2", 0.3], ["enemy", 0.2]]  # Wave 3
+]
+
 func _ready():
 	start_next_wave()
 
 func start_next_wave():
-	var wave_data = retrieve_wave_data()
-	await(get_tree().create_timer(0.2)).timeout # padding so waves dont insta start
-	spawn_enemies(wave_data)
+	if current_wave < waves.size():
+		var wave_data = retrieve_wave_data()
+		await get_tree().create_timer(0.2).timeout  # padding so waves don't start instantly
+		spawn_enemies(wave_data)
+	else:
+		print("All waves completed")
 
 func retrieve_wave_data():
-	var wave_data = [["enemy", 0.7], ["enemy_2", 0.1]]
+	var wave_data = waves[current_wave]
 	current_wave += 1
 	enemies_in_wave = wave_data.size()
 	return wave_data
@@ -26,6 +35,10 @@ func spawn_enemies(wave_data):
 			var new_enemy = skibidi.instantiate()
 			print(new_enemy)
 			add_child(new_enemy)
-			await(get_tree().create_timer(i[1])).timeout
+			await get_tree().create_timer(i[1]).timeout
 		else:
-			print("Failed to load scene: " + "res://Prefabs/" + i[0] + ".tscn")
+			print("Failed to load scene: res://Prefabs/" + i[0] + ".tscn")
+
+func start_next_wave_with_delay(delay):
+	await get_tree().create_timer(delay).timeout
+	start_next_wave()
